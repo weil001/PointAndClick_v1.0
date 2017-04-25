fb441f12969f9f1e4a6609b3c3e8ea843f9fd036
rstelly93 <stell003@cougars.csusm.edu>
2017-04-24 17:41:55 -0700

    Changes to ImportData page

--- PointAndClick_v1.0/ImportData.xaml.cs
+++ PointAndClick_v1.0/ImportData.xaml.cs
@@ -1,17 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Data;
+using System.Configuration;
+using System.Data.OleDb;
+using System.IO;
+using System.Windows.Forms;
+using FirebirdSql.Data.FirebirdClient;
+using System.Windows.Controls.Primitives;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Reflection;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PointAndClick_v1._0
 {
@@ -23,6 +25,68 @@ namespace PointAndClick_v1._0
         public ImportData()
         {
             InitializeComponent();
+            fillPostalMate();
         }
+
+        private void fillPostalMate()
+        {
+
+            string connectionString = "User=SYSDBA; Password=3k7rur9e; Database=PCS; DataSource=localhost; Port=3050;";
+
+            using (FbConnection con = new FbConnection(connectionString))
+            {
+                var Query = "SELECT rdb$field_name AS PRODUCT FROM rdb$relation_fields WHERE rdb$relation_name = 'PRODUCT'";
+                var dataAdapter = new FbDataAdapter(Query, con);
+                var commandBuilder = new FbCommandBuilder(dataAdapter);
+                DataTable dt = new DataTable();
+                dataAdapter.Fill(dt);
+                dataGrid1.ItemsSource = dt.DefaultView;
+            }
+
+        }
+
+        // Reads in a .csv file and stores contents in a datatable
+        //SELECT * FROM [{0}]
+        private DataTable ReadCSV(string fileName)
+        {
+            DataTable dt = new DataTable("Data");
+            using (OleDbConnection cn = new System.Data.OleDb.OleDbConnection("Provider=MIcrosoft.Jet.OLEDB.4.0;Data Source=\"" + Path.GetDirectoryName(fileName) + "\"; Extended Properties='text;HDR=yes;FMT=Delimeted(,)';"))
+            {
+                using (OleDbCommand cmd = new OleDbCommand(string.Format("SELECT * FROM [{0}]", new FileInfo(fileName).Name), cn))
+                {
+                    cn.Open();
+                    using (OleDbDataAdapter adapter = new OleDbDataAdapter(cmd))
+                    {
+                        adapter.Fill(dt);
+                    }
+                }
+            }
+            return dt;
+        }
+
+        //Browse button opens a file explorer and allows user to select a .csv file
+        private void loadCSV_Click(object sender, System.EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "CSV|*.csv", ValidateNames = true, Multiselect = false })
+            {
+                if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    dataGrid2.ItemsSource = ReadCSV(ofd.FileName).DefaultView;
+                }
+            }
+        }
+
+        //Browse button opens a file explorer and allows user to select a .csv file
+        private void CSV_Click(object sender, System.EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "CSV|*.csv", ValidateNames = true, Multiselect = false })
+            {
+                if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    dataGrid2.ItemsSource = ReadCSV(ofd.FileName).DefaultView;
+                }
+            }
+        }
+
     }
 }
